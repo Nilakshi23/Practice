56410a08cc3b978d8817072d868b38c04480b20e
Nilakshi23 <rao.nilaxshi@gmail.com>
2021-06-17 23:07:11 +0530

    re

---
+++ Vowel.java
@@ -0,0 +1,22 @@
+public class Vowel
+{
+     public static void main(String args[])
+	{
+          char ch='N';
+
+	     switch(ch)
+		{
+		  case 'A': System.out.println("Vowel A");
+					  break;
+		  case 'E': System.out.println("Vowel E");
+					  break;
+		  case 'I': System.out.println("Vowel I");
+					  break;
+		  case 'O': System.out.println("Vowel O");
+					  break;
+		  case 'U': System.out.println("Vowel U");
+					  break;
+		  default: System.out.println("Not a Vowel");
+	    }
+       }
+}
